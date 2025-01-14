b7b6f60857f994500ad1057ee54ee03137f2bcb5
Gustavo Pozzebon Lorenz <guguplorenz@gmail.com>
2025-01-14 16:59:26 +0000

    Teste inicial
    Teste inicial da API OpenWeather para verificação do clima da cidade

---
+++ Código/script.js
@@ -0,0 +1,9 @@
+var token = "bb81814f9a12e2b0090c1719a87aaf64"
+
+const funcao = async (cidade) =>{
+    var retorno = await fetch("https://api.openweathermap.org/data/2.5/weather?q="+cidade+"&units=metric&appid="+token+"&lang=pt_br")
+    json = await retorno.json()
+    console.log(json)
+}
+
+funcao("Caçapava do sul")
